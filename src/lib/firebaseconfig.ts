dfbea5d910039fe87095d5e244acacc1001dea4d
Matheus Corbellini <matheussteindorff@gmail.com>
2025-08-12 10:58:32 -0300

    arrumando firebase configuracao

--- src/lib/firebaseconfig.ts
+++ src/lib/firebaseconfig.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -9,4 +11,10 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 } as const;
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export { app };
+export default firebaseConfig;
